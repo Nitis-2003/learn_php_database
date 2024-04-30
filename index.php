855bd5b4080ee0063513447326600352a79bb44d
Nitis <nitis.182546@gmail.com>
2024-05-01 01:59:08 +0700

    add checkbox

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
         <form action="insertdata.php" method="post">
@@ -23,10 +25,27 @@
                 <input class="mt-2" type="radio" name="gender" value="male">ชาย
                 <input class="mt-2" type="radio" name="gender" value="female">หญิง
             </div>
+            <div>
+                <label class="mt-2" for="skill">ทักษะ</label>
+                <br>
+                <input class="mt-2" type="checkbox" name="skill[]" value="c++">C++
+                <br>
+
+                <input class="mt-2" type="checkbox" name="skill[]" value="python">Python
+                <br>
+
+                <input class="mt-2" type="checkbox" name="skill[]" value="php">PHP
+                <br>
+
+                <input class="mt-2" type="checkbox" name="skill[] " value="java">Java
+                <br>
+
+            </div>
             <input class="btn btn-success mt-3" type="submit" value="save">
             <input class="btn btn-danger mt-3" type="reset" value="reset">
         </form>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </html>
--- insertdata.php
+++ insertdata.php
@@ -3,9 +3,10 @@
  $fname = $_POST["fname"];
  $lname = $_POST["lname"];
  $gender = $_POST["gender"];
+ $skill = implode(",",$_POST["skill"]);
 
- $insert = "INSERT INTO employee(fname,lname,gender) VALUES('$fname','$lname','$gender')";
-//  echo $insert;
+ $insert = "INSERT INTO employee(fname,lname,gender,skill) VALUES('$fname','$lname','$gender','$skill')";
+ 
  $result = mysqli_query($con,$insert);
  if($result){
     echo "post success<br>";
